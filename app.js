3f3ca53fbb3a7027466b7a934aebef9a39e3f866
Jess Freasier <jfreasier@gmail.com>
2017-08-04 16:35:36 -0500

    initial take 2

---
+++ app.js
@@ -0,0 +1,15 @@
+const nunjucks = require('nunjucks')
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+app.listen(1337)
+
+
+// point nunjucks to the directory containing templates and turn off caching; configure returns an Environment
+// instance, which we'll want to use to add Markdown support later.
+var env = nunjucks.configure('views', {noCache: true});
+// have res.render work with html files
+app.set('view engine', 'html');
+// when res.render works with html files, have it use nunjucks to do so
+app.engine('html', nunjucks.render);
