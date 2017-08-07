d3f123cde61ea708cacad04fb2519ffad6d48e85
Jess Freasier <jfreasier@gmail.com>
2017-08-07 10:05:06 -0500

    done section 3

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const nunjucks = require('nunjucks')
 const bodyParser = require('body-parser')
 const express = require('express')
+const models = require('./models');
 const app = express()
-
-app.listen(1337)
-
 
 // point nunjucks to the directory containing templates and turn off caching; configure returns an Environment
 // instance, which we'll want to use to add Markdown support later.
@@ -13,3 +11,20 @@ var env = nunjucks.configure('views', {noCache: true});
 app.set('view engine', 'html');
 // when res.render works with html files, have it use nunjucks to do so
 app.engine('html', nunjucks.render);
+
+
+models.User.sync({})
+.then(function () {
+    return models.Page.sync({})
+})
+.catch(console.error);
+
+
+models.db.sync({force: true})
+.then(function () {
+    app.listen(1337, function () {
+        console.log('Server is listening on port 1337!');
+    })
+})
+.catch(console.error);
+
--- models/index.js
+++ models/index.js
@@ -1,32 +1,64 @@
 const Sequelize = require("sequelize");
 const db = new Sequelize("postgres://localhost:5432/wikistack");
 
+//to turn off logging, add 'logging: false' condition to new Sequelize instance
+
 const Page = db.define('page', {
   title: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   urlTitle: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   content: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   status: {
     type: Sequelize.ENUM("open", "closed")
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  }
+}, {
+  getterMethods: {
+    route() {
+      return '/wiki/' + this.urlTitle;
+    }
   }
 });
 
 const User = db.define('user', {
   name: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false
   },
   email: {
-    type: Sequelize.STRING
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    }
   }
 })
+
 
 model.exports = {
   Page: Page,
   User: User
 };
 
+
+// db.sync()
+//   .then(() => User.create({
+//     username: 'janedoe',
+//     birthday: new Date(1980, 6, 20)
+//   }))
+//   .then(jane => {
+//     console.log(jane.get({
+//       plain: true
+//     }));
+//   });
